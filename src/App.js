c087d0d89785aa6d6e355ce4311a8b128160d439
Fredrick Codero <fredrickcodero@gmail.com>
2023-09-11 19:30:08 +0300

    styled the cards

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import "./App.css";
-import WorkBoard from "./WorkBoard"
-import logo1 from "./logos/scoot.svg";
-import logo2 from "./logos/blogr.svg";
-import logo3 from "./logos/vector.svg";
-
+import WorkBoard from "./WorkBoard";
 
 function App() {
   return (
     <div className="logoContainer">
-     <WorkBoard img={logo1}/>
-      
+      <WorkBoard />
     </div>
   );
 }
--- src/WorkBoard.js
+++ src/WorkBoard.js
@@ -1,24 +1,30 @@
 import React from "react";
 import data from "./data/data.json";
+import images from "./data/data.json";
 
 function WorkBoard() {
   return (
-    <div className="container">
-      <div className="nav">
-        <h3>devjobs</h3>
-      </div>
-      <ul>
-        {data.map((item) => (
-          <div key={item.id}>
-            <img src=""/>
-            <p>{item.postedAt}</p>
-            <p>{item.contract}</p>
-            <h6>{item.position}</h6>
-            <p>{item.company}</p>
-            <h4>{item.location}</h4>
+    <div>
+      <body>
+        <div className="container">
+          <div className="nav">
+            <h3>devjobs</h3>
           </div>
-        ))}
-      </ul>
+
+          <div className="cards">
+            {data.map((item) => (
+              <div key={item.id} className="card">
+                <img src={item.logo} alt={item.company} />
+                <p>{item.postedAt}</p>
+                <p>{item.contract}</p>
+                <h6>{item.position}</h6>
+                <p>{item.company}</p>
+                <h4>{item.location}</h4>
+              </div>
+            ))}
+          </div>
+        </div>
+      </body>
     </div>
   );
 }
