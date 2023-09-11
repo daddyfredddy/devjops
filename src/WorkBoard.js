0276bb6dbf1e3b8021952f15cee32723cc540c14
Fredrick Codero <fredrickcodero@gmail.com>
2023-09-11 23:56:02 +0300

    final commit

--- src/WorkBoard.js
+++ src/WorkBoard.js
@@ -1,6 +1,8 @@
 import React from "react";
 import data from "./data/data.json";
 import images from "./data/data.json";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faBars} from "@fortawesome/free-solid-svg-icons";
 
 function WorkBoard() {
   return (
@@ -9,21 +11,24 @@ function WorkBoard() {
         <div className="container">
           <div className="nav">
             <h3>devjobs</h3>
+            <icon><FontAwesomeIcon icon={faBars} /></icon>
           </div>
 
           <div className="cards">
             {data.map((item) => (
               <div key={item.id} className="card">
-                <img src={item.logo} alt={item.company} />
-                <p>{item.postedAt}</p>
-                <p>{item.contract}</p>
-                <h6>{item.position}</h6>
-                <p>{item.company}</p>
-                <h4>{item.location}</h4>
+                <img src={item.logo} alt={item.company} className="img" />
+                <p id="text1">
+                  {item.postedAt} . {item.contract}
+                </p>
+                <h6 id="text3">{item.position}</h6>
+                <p id="text4">{item.company}</p>
+                <h4 id="text5">{item.location}</h4>
               </div>
             ))}
           </div>
         </div>
+        <button id="btn">Load More</button>
       </body>
     </div>
   );
